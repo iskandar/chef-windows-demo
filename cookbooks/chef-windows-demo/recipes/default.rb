a3baaa6b40351c9d76b990d957be45eaa2be3191
Iskandar Najmuddin <iskandar.najmuddin@rackspace.co.uk>
2016-02-28 19:08:33 +0000

    Add initial cookbook files

---
+++ cookbooks/chef-windows-demo/recipes/default.rb
@@ -0,0 +1,51 @@
+#
+# Cookbook Name:: chef-windows-demo
+# Recipe:: default
+#
+#
+
+# Stop the default site
+iis_site 'Default Web Site' do
+  action [:stop]
+end
+
+# Create a new directory.
+# We want this to be empty so our Load Balancer does not add this node into rotation.
+directory "#{node['iis']['docroot']}/WebApplication1" do
+  action :create
+end
+
+# Create a new IIS Pool
+iis_pool 'WebApplication1' do
+  runtime_version "4.0"
+  pipeline_mode :Integrated
+  pool_identity :ApplicationPoolIdentity
+  load_user_profile true
+  action :add
+end
+
+# Create a new IIS site
+iis_site 'WebApplication1' do
+  protocol :http
+  port 80
+  path "#{node['iis']['docroot']}/WebApplication1"
+  application_pool 'WebApplication1'
+  action [:add,:start]
+end
+
+service 'w3svc' do
+  action [:enable, :start]
+end
+
+include_recipe 'webpi'
+
+webpi_product 'WDeployPS' do
+  accept_eula true
+  action :install
+end
+
+# octopus_deploy_tentacle 'Tentacle' do
+#   action [:install, :configure]
+#   version '3.2.24'
+#   trusted_cert "#{node['octopus']['tentacle']['instance']['trusted_cert']}"
+# end
