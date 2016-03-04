e785d491e7b978caa50e6e283fe58b1737770373
Iskandar Najmuddin <iskandar.najmuddin@rackspace.co.uk>
2016-03-04 00:08:26 +0000

    Bump version to 0.2.19

--- cookbooks/chef-windows-demo/metadata.rb
+++ cookbooks/chef-windows-demo/metadata.rb
@@ -5,7 +5,7 @@ license          'Apache 2.0'
 description      'Installs and configures IIS, the W3SVC, WDeployPS'
 long_description 'Installs and configures IIS, the W3SVC, and WDeployPS'
 supports         'windows'
-version          '0.2.18'
+version          '0.2.19'
 
 depends 'webpi'
 depends 'iis'
